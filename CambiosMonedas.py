a3ebb3d7e1f98ee20039c8d3e94ac2c0deaa1b0a
fray osorio <frayosorio@gmail.com>
2025-04-10 13:29:23 -0500

    Se completa el calculo y despliegue de las estadististicas de manera FUNCIONAL

--- CambiosMonedas.py
+++ CambiosMonedas.py
@@ -6,6 +6,8 @@ import Util
 import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
+from functools import reduce
+import math
 
 iconos = ["./iconos/Grafica.png", "./iconos/Datos.png"]
 textos = ["Gráfica Fecha vs Cambio", "Calculas Estadísticas"]
@@ -67,11 +69,131 @@ def mostrarGrafica():
         plt.grid()
         plt.legend()
 
-        plt.savefig("graficaCambiosMoneda.png")
+        #guardar la grafica
+        nombreArchivo="graficaCambiosMoneda.png"
+        plt.savefig(nombreArchivo)
+        #cargar la grafica
+        imgGrafica = PhotoImage(file = nombreArchivo)
+
+        #crear LABEL para mostrar la grafica en el primer panel
+        lblGrafica=Label(paneles[0])
+        lblGrafica.config(image=imgGrafica)
+        lblGrafica.image=imgGrafica
+
+        lblGrafica.place(x=0, y=0)
+
+        #redimensionar la ventana
+        ventana.minsize(imgGrafica.width(), imgGrafica.height()+100)
+
+        #seleccionar el panel respectivo
+        nb.select(0)
+
+def extraerCambios(datos):
+    return list(map(lambda dato: dato["cambio"], datos))
         
+#calculos de las estadisticas
+def calcularPromedio(cambios):
+    return reduce(lambda suma, cambio: suma + cambio, cambios) / len(cambios) if cambios else 0
+
+    #funcional Pythonica
+    return sum(cambios) / len(cambios) if cambios else 0
+
+    #No funcional
+    suma = 0
+    if cambios:
+        for cambio in cambios:
+            suma += cambio
+    return suma / len(cambios)
+
+def calcularDesviacionEstandar(cambios):
+    promedio = calcularPromedio(cambios)
+    return math.sqrt(reduce(lambda suma, cambio: suma + (cambio-promedio)**2, cambios, 0) / len(cambios) if cambios else 0)
+
+    #No funcional
+    desviacion = 0
+    if cambios:
+        promedio = calcularPromedio(cambios)
+        suma = 0
+        for cambio in cambios:
+            suma += (cambio-promedio)**2
+        desviacion = math.sqrt(suma / len(cambios))
+    return desviacion
+
+def calcularMaximo(cambios):
+    return reduce(lambda maximo, cambio: cambio if cambio>maximo else maximo, cambios) if cambios else 0
+
+def calcularMinimo(cambios):
+    return reduce(lambda minimo, cambio: cambio if cambio<minimo else minimo ,cambios) if cambios else 0
+
+def calcularModa(cambios):
+    #hallar la frecuencia de cada dato
+    frecuencias = reduce(lambda diccionarios, cambio: {**diccionarios, cambio: diccionarios.get(cambio, 0) + 1}, cambios, {}) 
+    #hallar la mayor frecuencia
+    frecuenciaMaxima = reduce(lambda maximo, frecuencia: frecuencia if frecuencia[1]>maximo[1] else maximo, frecuencias.items())
+    return frecuenciaMaxima[0] if frecuenciaMaxima[1] > 1 else None
+
+    #funcional Pythonica
+    try:
+        return mode(cambios) if cambios else 0
+    except statistics.StatisticsError:
+        return None
+
+def calcularEstadisticas(moneda, desde, hasta):
+    datos = obtenerDatos()
+    datosFiltrados = filtrarDatos(datos, moneda, desde, hasta)
+    cambios = extraerCambios(datosFiltrados)
+
+    moda = calcularModa(cambios)
+    return {
+        "Promedio:" : calcularPromedio(cambios),
+        "Desviación estandar:": calcularDesviacionEstandar(cambios),
+        "Máximo:": calcularMaximo(cambios),
+        "Mínimo:": calcularMinimo(cambios),
+        "Moda:": moda if moda else "No hay moda"
+        }
 
 def mostrarEstadisticas():
-    messagebox.showinfo("", "Hizo clic en ESTADISTICAS")
+    if cmbMoneda.current() >= 0:
+        #obtener datos de entrada
+        moneda = monedas[cmbMoneda.current()]
+        desde = datetime.strptime(cldDesde.get(), "%d/%m/%Y")
+        hasta = datetime.strptime(cldHasta.get(), "%d/%m/%Y")
+        
+        estadisticas = calcularEstadisticas(moneda, desde, hasta)
+        #mostrar resultados
+        for i, (clave, valor) in enumerate(estadisticas.items()):
+            Util.agregarEtiqueta(paneles[1], clave, i, 0)
+            Util.agregarEtiqueta(paneles[1], valor, i, 1)
+
+        #seleccionar el panel respectivo
+        nb.select(1)
+        return
+        
+
+        #No funcional
+        #mostrar el promedio
+        Util.agregarEtiqueta(paneles[1], "Promedio:", 0, 0)
+        Util.agregarEtiqueta(paneles[1], calcularPromedio(cambios), 0, 1)
+
+        #mostrar la desviación estandar
+        Util.agregarEtiqueta(paneles[1], "Desviación estandar:", 1, 0)
+        Util.agregarEtiqueta(paneles[1], calcularDesviacionEstandar(cambios), 1, 1)
+
+        #mostrar el máximo
+        Util.agregarEtiqueta(paneles[1], "Máximo:", 2, 0)
+        Util.agregarEtiqueta(paneles[1], calcularMaximo(cambios), 2, 1)
+
+        #mostrar el mínimo
+        Util.agregarEtiqueta(paneles[1], "Mínimo:", 3, 0)
+        Util.agregarEtiqueta(paneles[1], calcularMinimo(cambios), 3, 1)
+
+        #mostrar la moda
+        moda = calcularModa(cambios)
+        Util.agregarEtiqueta(paneles[1], "Moda:", 4, 0)
+        Util.agregarEtiqueta(paneles[1],  moda if moda else "No hay moda"  , 4, 1)
+
+
+        
 
 ventana = Tk()
 ventana.title("Cambios de Moneda")
@@ -94,6 +216,8 @@ cldHasta = Util.agregarCalendario(frmMoneda, 0, 3)
 nb = Notebook(ventana)
 nb.pack(fill=BOTH, expand=YES)
 pestañas = ["Gráfica", "Estadisticas"]
+paneles = []
 for p in pestañas:
     frm = Frame(ventana)
+    paneles.append(frm)
     nb.add(frm, text=p)
